9b37e6fecc192062c5632145d303247cd1d2f6cb
manulthieme <manuelthieme@googlemail.com>
2014-03-07 21:16:59 +0100

    init Project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: morion
+ *
+ * Created on 7. März 2014, 21:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
